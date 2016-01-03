1bf8922a0d55dde4f74c4920b1900d5090372347
David Personette <dperson@gmail.com>
2016-01-03 08:17:10 -0500

    torproxy.sh allow any GID/UID to be set (even dupes)

--- torproxy.sh
+++ torproxy.sh
@@ -108,8 +108,8 @@ shift $(( OPTIND - 1 ))
 [[ "${TZ:-""}" ]] && timezone "$TZ"
 [[ "${SERVICE:-""}" ]] && eval hidden_service \
             $(sed 's/^\|$/"/g; s/;/" "/g' <<< $SERVICE)
-[[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID debian-tor
-[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID debian-tor
+[[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o debian-tor
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o debian-tor
 
 chown -Rh debian-tor. /var/lib/tor /var/log/tor 2>&1 | grep -iv 'Read-only' || :
 
